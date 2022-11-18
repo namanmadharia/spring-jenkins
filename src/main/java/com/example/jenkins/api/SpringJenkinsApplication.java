d604ea8c94160d4116bdfee96b1dea398b5b0777
Naman Madharia <naman.madharia@pisystindia.com>
2022-11-18 13:17:11 +0530

    first commit

---
+++ src/main/java/com/example/jenkins/api/SpringJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.example.jenkins.api;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static org.slf4j.Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	public void inti() {
+		logger.info("App started...");
+	}
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		System.out.println("Runnnig");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
